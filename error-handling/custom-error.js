b03d591773673f1b37e373b6c4134c83ae9f818c
Shankar <shangan.23@gmail.com>
2021-04-23 11:24:34 +0530

    tech(error-handling): add output to code

--- error-handling/custom-error.js
+++ error-handling/custom-error.js
@@ -1,11 +1,9 @@
 // Simple Error handling example
 try {
-	   throw new Error("My Custom Error Message Here"); // Should throw Reference Error since we are calling an undefined variable
-	 }
-catch(e) {
-	console.log("Error Name: " + e.name);
-	console.log("Error message:  " + e.message);
-}
-finally {
-	console.log("This should print anyway regardless of exception ");
+  throw new Error('My Custom Error Message Here') // Should throw Reference Error since we are calling an undefined variable
+} catch (e) {
+  console.log('Error Name: ' + e.name) //Error Name: Error
+  console.log('Error message:  ' + e.message) //Error message:  My Custom Error Message Here
+} finally {
+  console.log('This should print anyway regardless of exception ') //This should print anyway regardless of exception
 }
--- error-handling/error-object.js
+++ error-handling/error-object.js
@@ -1,5 +1,5 @@
 // Builtin Error Object
 
-let err = new Error("Oops Something Went Wrong");
-console.log(err.name);  // prints constructor name used to create this object. i.e Error
-console.log(err.message); // Prints the message passed to the Error constructor
+let err = new Error('Oops Something Went Wrong')
+console.log(err.name) // Error
+console.log(err.message) // Oops Something Went Wrong
