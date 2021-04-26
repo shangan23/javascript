cac010ef803d302b67b8b6db4de8bfe610035868
Shankar <shangan.23@gmail.com>
2021-04-26 15:47:34 +0530

    tech(array): array exercise

---
+++ array/exercise.js
@@ -0,0 +1,5 @@
+let fruits = ["Strawberry", "Mango"]
+let sliceFruits = fruits.slice(0,1) // to do shallowCopy
+console.log(sliceFruits, fruits)
+let spliceFruits = fruits.splice(0,1) // to remove item in an array
+console.log(spliceFruits, fruits)
